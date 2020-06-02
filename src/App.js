eb8a4a4576507f1003dc16e22abfe35ab3082ec2
Bhavna Jain <bhavnajain_16@hotmail.com>
2020-06-02 18:07:55 +0530

    [ADD] Initial Layout for the app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React from 'react';
 
+import Button from 'react-bootstrap/Button'
+
 import QuizQuestions from './components/QuizQuestions';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <QuizQuestions />
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quizzStarted: false
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {
+            this.state.quizzStarted ?
+              <QuizQuestions />
+            : 
+            <div className="text-center">
+              <h2>Hello, Welcome to <b>Quizz App</b>!!</h2>
+              <Button 
+                variant="outline-danger" 
+                onClick={() => {
+                  this.setState({
+                    quizzStarted: true
+                  });
+                }}
+              >
+                <i>Start the Quizz</i>
+              </Button>
+            </div>
+          }
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
